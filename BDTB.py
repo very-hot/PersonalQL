7bc3c13f74262907bae4f021d2022c5c7b9396f8
very-hot <huangxzmr@gmail.com>
2025-03-26 16:21:06 +0800

    百度贴吧

--- BDTB.py
+++ BDTB.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     config = {
         "TIEBA": [
             {
-                "cookie": TIEBA_NAME
+                "cookie": "TIEBA_NAME"
             }
         ]
     }
